cf15c7814058799ead9b377a384a8d949832331b
khh-Niloy <niloy.dev.101@gmail.com>
2025-04-29 20:57:17 +0600

    add /generate-text route with Gemini 2.0 Flash integration

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,34 @@
+import dotenv from "dotenv";
 import express from "express";
-const app = express();
+import cors from "cors";
+import { GoogleGenAI } from "@google/genai";
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("all okay");
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.post("/generate-text", (req, res) => {
+  const ai = new GoogleGenAI({ apiKey: `${process.env.GEMINI_API_KEY}` });
+
+  async function main() {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: "Explain how AI works in a few words",
+    });
+    console.log(response.text);
+  }
+
+  main();
 });
+
+// app.get("/", (req, res) => {
+//   res.send("all okay");
+// });
 
 app.listen(8000, () => {
   console.log("Server is running at 8000");
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+
+  async function formInput(data) {
+    console.log(data);
+    const res = await axios.post("http://localhost:8000/generate-text", data);
+    console.log(res);
+  }
+
   return (
     <>
       <div className="vite-hero-bg">
@@ -21,29 +28,32 @@ function App() {
             <br /> that leave a strong impression on employers.
           </p>
 
-          <div className="flex gap-10 mt-20 w-[60%]">
-            <textarea
-              rows={10}
-              placeholder="Paste the job description here..."
-              className="border-b placeholder:text-white/70 custom-scrollbar focus:outline-0 place-content-end resize-none
-               text-white/90 bg-black/10 p-4 rounded-t-xl border-white/70 w-1/2"
-            />
-            <textarea
-              rows={10}
-              placeholder="Paste your resume content here..."
-              className="border-b placeholder:text-white/70 custom-scrollbar focus:outline-0 place-content-end resize-none
-               text-white/90 bg-black/10 p-4 rounded-t-xl border-white/70 w-1/2"
-            />
-          </div>
-
-          <div className="py-10">
-            <button
-              className="bg-gradient-to-tl from-[#BB5EE9] via-[#8258B6] to-[#6ab9ea] text-white 
-            font-semibold py-3 px-3 rounded-md border-t-[1px] border-white shadow-2xl hover:scale-[1.05] duration-300 transition-transform active:scale-110"
-            >
-              Generate Cover Letter
-            </button>
-          </div>
+          <form onSubmit={handleSubmit(formInput)} className="w-[60%]">
+            <div className="flex gap-10 mt-20">
+              <textarea
+                {...register("jobDescription")}
+                rows={10}
+                placeholder="Paste the job description here..."
+                className="border-b placeholder:text-white/70 custom-scrollbar focus:outline-0 place-content-end resize-none
+                 text-white/90 bg-black/10 p-4 rounded-t-xl border-white/70 w-1/2"
+              />
+              <textarea
+                rows={10}
+                {...register("resumeContent")}
+                placeholder="Paste your resume content here..."
+                className="border-b placeholder:text-white/70 custom-scrollbar focus:outline-0 place-content-end resize-none
+                 text-white/90 bg-black/10 p-4 rounded-t-xl border-white/70 w-1/2"
+              />
+            </div>
+            <div className="py-10 text-center">
+              <button
+                className="bg-gradient-to-tl from-[#BB5EE9] via-[#8258B6] to-[#6ab9ea] text-white 
+            font-semibold py-3 px-3 rounded-md border-t-[1px] border-white shadow-2xl hover:scale-[1.05] duration-300 transition-transform active:scale-105"
+              >
+                Generate Cover Letter
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
