29b504369897308ba5e616535f22c0df04316586
khh-Niloy <niloy.dev.101@gmail.com>
2025-05-01 02:55:14 +0600

    fixing cors (6)

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,14 @@ import path from "path";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use(
-  "/generated_files",
-  express.static(path.join(process.cwd(), "generated_files"))
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const launchBrowser = async () => {
   const browser = await puppeteer.launch({ headless: true });
